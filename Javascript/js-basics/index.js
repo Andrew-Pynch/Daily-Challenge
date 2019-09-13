79456f9daf352fa3378a45bfe7da62315c435503
Andrew <andrewpynchbuisness@gmail.com>
2019-09-13 14:12:20 -0700

    learning some js

---
+++ Javascript/js-basics/index.js
@@ -0,0 +1,13 @@
+
+function greet(name, age) {
+    console.log('Hello ' + name + ' you are ' + age);
+};
+
+
+
+// Calculation
+function square(number) {
+    return number * number;
+} 
+
+console.log(square(2));
