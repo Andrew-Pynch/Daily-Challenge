d7fb193bb0382fa853d795aec18848ad7996832a
Pynch <Andrew.Pynch@cppcorp.com>
2019-10-27 15:45:47 -0700

    .

--- C++/CS161/Assignments/assignment2.cpp
+++ C++/CS161/Assignments/assignment2.cpp
@@ -12,6 +12,7 @@ Description: Simple program to calculate take home pay after deductions and taxe
 
 using namespace std; // now we don't have to affix inputs / outputs with std::cout etc...
 
+
 /*
 Function: check_range()
 Description: Tests if test_value is in range of lower, and upper bound
@@ -39,25 +40,123 @@ Parameters: char
 Pre-Conditions: Char input
 Post-Conditions: bool indicating whether input was upper or not
 */
-// CHECK IF CHARACTER IS UPPER
 bool is_capital(char letter)
 {
     if (letter >= 'A' && letter <= 'Z')
     {
-        cout << false;
+        return true;
     }
     else
     {
-        cout << true;
+        return false;
     }
 }
 
 
+/*
+Function: is_even()
+Description: checks if input is even
+Parameters: int
+Pre-Conditions: int input 
+Post-Conditions: bool
+*/
 
 
+/*
+Function: is_odd
+Description: checks if input is odd
+Parameters: int
+Pre-Conditions: int input 
+Post-Conditions: bool
+*/
+
+
+/*
+Function: equality test
+Description: checks if two numbers are the same or different and returns different values accordingly
+Parameters: num1, num2
+Pre-Conditions: int input
+Post-Conditions: -1, 0 , 1
+*/
+
+
+/*
+Function: is_int
+Description: checks if string is int
+Parameters: string num
+Pre-Conditions: string input
+Post-Conditions: bool
+*/
+
+
+/*
+Function: numbers_present
+Description: Checks if a string contains numbers
+Parameters: string sentence
+Pre-Conditions: string input
+Post-Conditions: bool
+*/
+
+
+/*
+Function: letters present
+Description: Checks if a string contains numbers
+Parameters: string sentence
+Pre-Conditions: string input
+Post-Conditions: bool
+*/
+
+
+/*
+Function: contains sub string
+Description: checks if a substring exists in a sentence
+Parameters: string sentence, string substring
+Pre-Conditions: string input
+Post-Conditions: bool
+*/
+
+
+/*
+Function: word count
+Description: count how many words are in a given string
+Parameters: string sentence
+Pre-Conditions: string input
+Post-Conditions: int
+*/
+
+
+/*
+Function: to_upper
+Description: converts all letters in a string to uppercase
+Parameters: string sentence
+Pre-Conditions: string input
+Post-Conditions: uppercase sentence
+*/
+
+
+/*
+Function: to lower
+Description: converts all letters in a string to lowercase
+Parameters: string sentence
+Pre-Conditions: string input
+Post-Conditions: lowercase sentence
+*/
+
+
+/*
+Function: get int
+Description: takes string input and checks if its an int and if it is returns aforementioned int
+Parameters: string prompt
+Pre-Conditions: string input from user
+Post-Conditions: int
+*/
 
 int main()
 {
-    is_capital('a');
+
+
+
+
+
 }
 
