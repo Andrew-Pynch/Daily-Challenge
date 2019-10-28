8b2db04286d37d9db940ac5d9c29ca0686f134f1
Trapezoider <43330642+Trapezoider@users.noreply.github.com>
2019-10-28 09:19:51 -0700

    Update reverse_int.py

--- Python/November_2019/reverse_int.py
+++ Python/November_2019/reverse_int.py
@@ -1,8 +1,11 @@
 def reverse_int(num):
     num = str(num)
     num = list(num)
+
     print("Input: " + str(num))
+
     reversed_num = []
+    
     for i in range(len(num)):
         current = num.pop()
         reversed_num.append(current)
