b1725f2c28f309d75b03dd39bbd85aa2b40efb4a
Pynch <Andrew.Pynch@cppcorp.com>
2019-10-31 09:02:46 -0700

    Update min_range_needed_to_sort.py

--- Python/November_2019/min_range_needed_to_sort.py
+++ Python/November_2019/min_range_needed_to_sort.py
@@ -3,9 +3,12 @@ def find_range(nums):
 
     range_start = 0
     range_end = 0
-    for i in range(len(nums)):
+    for i in range(len(nums) - 1):
         if nums[i + 1] < nums[i]:
             range_end = i
-            break
+            continue
+            
 
-    print
+    print("Minimum range needed to sort lies between %s and %s" % (range_start, range_end))
+
+find_range([1, 7, 9, 5, 7, 8, 10])
