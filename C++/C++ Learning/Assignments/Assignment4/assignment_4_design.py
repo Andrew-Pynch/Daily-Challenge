4c629d1488c55c2342d9282fa2ec48930b39bad6
Andrew <andrewpynchbusiness@gmail.com>
2019-11-13 13:24:00 -0800

    done for now. figure out how to implement count function

--- C++/C++ Learning/Assignments/Assignment4/assignment_4_design.py
+++ C++/C++ Learning/Assignments/Assignment4/assignment_4_design.py
@@ -8,7 +8,7 @@ def palindrome_detector(string):
     string = list(string) # make each character an entry in a list
     
     # Remove spaces
-    for i in range(string):
+    for i in range(len(string)):
         if string[i] == " ": # space
             string[i].pop
 
@@ -19,7 +19,21 @@ def palindrome_detector(string):
     else:
         return False;
 
-    
 
+def word_frequency(string):
+    # Turn input string into a list of words
+    list_words = list_words.split()
+
+    word_frequency = []
+
+
+    # TODO
+    # HOW AM I GOING TO IMPLEMENT A COUNT FUNCTION?
+    for i in list_words:
+        word_frequency.append(list_words.count(i))
+
+    
+        
+    
 
         
