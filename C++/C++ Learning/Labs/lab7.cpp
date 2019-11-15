4fecf3d5bf0701556de61820b3bb125a4220e386
Trapezoider <43330642+Trapezoider@users.noreply.github.com>
2019-11-15 03:33:34 -0800

    Create lab7.cpp

---
+++ C++/C++ Learning/Labs/lab7.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// GET_STRING
+// makes a copy of the string, gives it back to main
+void get_string(string *str)
+{
+       cout << "Please enter your string: " << endl;
+       getLine(cin, str);
+}
+
+
+
+// SET_REPLACE_STRING
+// Changes all non-space letters to dashes
+void set_replace_string(string, string *){
+    for (int i = 0; i < string.length(); i++){
+        if (int(string.at(i)) != 32){
+            *copy += char(45);
+        }
+        else{
+                               
+                        
+                        
+                        
+                        
+
+ 
+
+
+
+
+// Searches a replaced string for a letter and displays letter
+// replaced_string = "----"
+void get_search_replace(string *)
+{
+    cout << "Which letter would you like to see? " << endl;
+}
+                        
+                        
+
+
+
+int main() //Copy string
+{
+    string *original;
+        
+    get_string(original);
+    
+    
+    // COPY STRING 
+    string *copy = original;
+    
+    
+    // FUNCTION CALLS
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
