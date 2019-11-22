f63b53324d1c93c770ff12d2eb3bc144f6fb63b4
Andrew <andrewpynchbusiness@gmail.com>
2019-11-22 12:41:02 -0800

    stopping assignment work for now.

---
+++ C++/C++ Learning/Assignments/Assignment4/assignment4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+char * purge_string(char * str)
+{
+
+}
+
+bool is_palindrome(char *str)
+{
+
+}
+
+
+
+
+
+
+int main()
+{
+    cout << "Please enter a string: ";
+    purge_string(cin); 
+    return 0;
+}
