920f6cfc0852bfa2744d82e9792edaf60dca67a7
Andrew <andrewpynchbusiness@gmail.com>
2019-11-24 07:11:53 -0800

    making progress ass-4

--- C++/C++ Learning/Assignments/Assignment4/assignment4.cpp
+++ C++/C++ Learning/Assignments/Assignment4/assignment4.cpp
@@ -1,27 +1,50 @@
+/*
+Program: assignmen43.cpp - CS161
+Author: Andrew Pynch - pyncha@oregonstate.edu
+Student ID: 932-953-192
+Description: Detect Palindromes, Count word frequency, count frequency of given word
+*/
 #include <iostream>
 #include <string>
-#include <string.h>
+#include <cstring>
+#include <cmath>
 
 using namespace std;
 
-char * purge_string(char * str)
+// Get c - style string
+void get_string(char *str)
 {
-
+    cout << "Please enter a string: ";
+    cin.getline(str, 1025);
 }
 
-bool is_palindrome(char *str)
+void purge_string_space(char *dirty_string, char *clean_string)
 {
-
+    
+    for (int i = 0; i < strlen(dirty_string); i++)
+    {
+        cout << "HI" << endl;
+    } 
 }
-
-
 
 
 
 
 int main()
 {
-    cout << "Please enter a string: ";
-    purge_string(cin); 
-    return 0;
+    char *input = new char;
+    get_string(input);
+
+    /*
+    cout << "Content of input: " << *input << endl;
+    cout << "Address input points to: " << input << endl;
+    cout << "Address of input: " << &input << endl << endl;
+
+    cout << strlen(input) << endl;
+    */
+
+   purge_string_space(input);
+
+
+   return 0;
 }
